784336e5304519d9c522f1a3474d8904c9acf2e3
Akash <vyasishanatc194@gmail.com>
2021-04-21 18:12:37 +0530

    Updated teh hellper.py file

--- django_boilerplate/helper.py
+++ django_boilerplate/helper.py
@@ -3,7 +3,7 @@ import os
 from sendgrid.helpers.mail import Email, Substitution, Mail, Personalization
 from python_http_client import exceptions
 from .settings import SENDGRID_API_KEY
-from .temlplate_id import DJANGO_BOILERPLATE_TEST_TEMPLATE_ID
+from .template_id import DJANGO_BOILERPLATE_TEST_TEMPLATE_ID
 
 # to_email - Whom to send the email
 # sendgrid_temlpate_id - Template id created in the sendgrid account for different template
@@ -12,8 +12,7 @@ from .temlplate_id import DJANGO_BOILERPLATE_TEST_TEMPLATE_ID
 #                           'variable_name_as_in_sendgrid_tempaltes': value in string format,
 #                           }
 # Example dynamic_data_for_template = {
-            #     'subject': 'Testing the SendGrid HTML templte Email',
-            #     'message':'Hello, Successfully sent the SendGrtid template email'
+            #     'name': 'Citrusbug Team',
             # }
 
 def send_html_template_email(to_email, sendgrid_temlpate_id, dynamic_data_for_template):
@@ -22,7 +21,7 @@ def send_html_template_email(to_email, sendgrid_temlpate_id, dynamic_data_for_te
     personalization = Personalization()
     personalization.add_to(Email(to_email))
     mail = Mail()
-    mail.from_email = Email("akash.citrusbug@gmail.com")
+    mail.from_email = Email("citrusbugteam@gmail.com")
     mail.template_id = sendgrid_temlpate_id
     personalization.dynamic_template_data = dynamic_data_for_template
     mail.add_personalization(personalization)
