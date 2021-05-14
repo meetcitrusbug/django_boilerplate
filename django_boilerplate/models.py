5c29dab7e084970d08f550bab00841748c788373
rahul <rahul.citrusbug@gmail.com>
2021-05-14 16:49:58 +0530

    removed unused code

--- django_boilerplate/models.py
+++ django_boilerplate/models.py
@@ -1,9 +1,7 @@
 import uuid
 from django.db import models
-from django.shortcuts import reverse
 from django.utils.translation import gettext as _
-from django.contrib.auth.models import BaseUserManager
-from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.models import AbstractUser
 
 class ActivityTracking(models.Model):
     
