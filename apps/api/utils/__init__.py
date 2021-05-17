97549e4910d850ed2e5d1eead053b20418f461c0
vyasishanatc194 <vyasishanatc194@gmail.com>
2021-05-17 10:42:33 +0530

    Removed unwanted code and Postman collection updated!

--- apps/api/utils/__init__.py
+++ apps/api/utils/__init__.py
@@ -6,5 +6,4 @@ These imported functions can be directly accessible throughout the Application.
 This __init__ file works same as a default __init__ method in any class OR function.
 """
 
-from .exception import CustomValidation
 from .api import get_status, modify_api_response
